81d15808832ca25af9a780d2b6df07e3575ab93b
vyen <valmyen@icloud.com>
2025-03-31 14:17:08 -0400

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import '../stylesheets/About.css';
 const About = () => {
     return (
         <div className="About">
-            <img src="public/headshot.png" className="headshot"/>
+            <img src="headshot.png" className="headshot"/>
             <p className="blurb">Hi y’all! <br></br><br></br>
                 My name is Valerie Yen (she/they) and I’m an undergraduate student at the University of Maryland. I’m pursuing a Bachelor of Science in Immersive Media Design and Information Science. With that degree, I want to pursue a career in Full Stack development working in the entertainment industry. <br></br><br></br>
                 In my free time, I like to cook, go to the gym, and perform with the Gamer Symphony Orchestra!</p>
